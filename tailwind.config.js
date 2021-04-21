a6203c625de64b64dd571e4446904602334b7430
Amita-Roy <sardana.amita2@gmail.com>
2021-04-21 16:08:25 +0200

    Add tailwind configuration for custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#d3ae6b',
+        dark: '#6e5e3d',
+      },
+      black: colors.black,
+      white: colors.white,
+    },
     extend: {},
   },
   variants: {
