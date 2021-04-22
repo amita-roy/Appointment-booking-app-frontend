374ad7b9f6aa533ede2557ac96870380161aeae8
Amita-Roy <sardana.amita2@gmail.com>
2021-04-22 09:42:32 +0200

    Add Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer h-[400px]">
+    <div className="footer-top bg-black text-primary h-[350px]">Top</div>
+    <div className="footer-bottom bg-primary-dark h-[50px] flex items-center justify-center">
+      <p className="text-sm font-light text-primary-light">
+        Copyright Encapture 2021 - Terms & Conditions Privacy Policy
+      </p>
+    </div>
+  </div>
+);
+
+export default Footer;
