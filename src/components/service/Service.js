3ef5d8c624dc5e6ae302933571686bc29b33a828
Amita-Roy <sardana.amita2@gmail.com>
2021-04-23 17:46:55 +0200

    Update the transition duration

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Service = () => (
-  <div className="w-80 border-2 py-4 px-5 lg:w-96 border-opacity-50 border-black shadow-xl bg-white cursor-pointer transition duration-700 hover:transform hover:scale-105">
+  <div className="w-80 border-2 py-4 px-5 lg:w-96 border-opacity-50 border-black shadow-xl bg-white cursor-pointer transition duration-300 hover:transform hover:scale-105">
     <div className="flex justify-center image-wrapper">
       <div className="h-36 w-36 object-cover object-center rounded-full overflow-hidden">
         <img
