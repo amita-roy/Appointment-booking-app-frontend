b03b7ac12b16cd958fc744eee225161fb76be002
Amita-Roy <sardana.amita2@gmail.com>
2021-04-23 10:23:15 +0200

    Update pages folder

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'pages/homepage/HomePage';
+import HomePage from 'pages/HomePage';
 
 const Routes = () => (
   <Switch>
