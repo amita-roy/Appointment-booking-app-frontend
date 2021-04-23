8139c1957fddb4c6b39ea7f6f68943e711384945
Amita-Roy <sardana.amita2@gmail.com>
2021-04-23 12:10:59 +0200

    Create services page

---
+++ src/pages/ServicesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Services from 'container/header/services/Services';
+
+const ServicesPage = () => (
+  <div className="services-page p-10">
+    <h1>Services Page</h1>
+    <Services />
+  </div>
+);
+
+export default ServicesPage;
