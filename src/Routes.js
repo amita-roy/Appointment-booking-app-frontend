9f914837d3919ddabad9f6a39f60a8cd022a6410
Amita-Roy <sardana.amita2@gmail.com>
2021-04-23 12:10:10 +0200

    Add route for services page

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'pages/HomePage';
+import ServicesPage from 'pages/ServicesPage';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={HomePage} />
+    <Route path="/services" exact component={ServicesPage} />
   </Switch>
 );
 
