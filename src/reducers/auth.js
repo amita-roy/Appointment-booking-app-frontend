2a5a39793b20324ffa324fa095c99c6ec936a550
Amita-Roy <sardana.amita2@gmail.com>
2021-04-26 10:28:17 +0200

    Add auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,13 +2,14 @@ import ACTIONS from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   authenticated: '',
+  user: {},
   errors: [],
 };
 
 const auth = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTIONS.AUTH_USER:
-      return { ...state, authenticated: action.payload };
+      return { ...state, authenticated: action.payload, user: action.user };
     case ACTIONS.AUTH_ERR:
       return { ...state, errors: action.payload };
 
