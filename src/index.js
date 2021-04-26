580b3d4af3f22e6839cad590a3dce6471e381a27
Amita-Roy <sardana.amita2@gmail.com>
2021-04-26 09:50:42 +0200

    Add provider, middleware and store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
+import rootReducer from 'reducers';
 import App from 'components/app/App';
 import './index.css';
 
+const initialState = {
+  auth: { authenticated: localStorage.getItem('token') },
+};
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk)),
+);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
