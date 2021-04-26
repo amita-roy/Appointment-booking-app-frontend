ac3859ce366de36b2b18d32a0b4556ff202cd650
Amita-Roy <sardana.amita2@gmail.com>
2021-04-26 17:55:28 +0200

    Fetch all the services from backend to services container

--- src/container/Services.js
+++ src/container/Services.js
@@ -1,22 +1,32 @@
 import Service from 'components/service/Service';
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import * as Actions from 'actions';
 
 class Services extends PureComponent {
+  componentDidMount() {
+    const { fetchAllServices } = this.props;
+    fetchAllServices();
+  }
+
   render() {
+    const { services } = this.props;
     return (
       <div className="grid grid-cols-2 xl:grid-cols-3 gap-12">
-        <Service />
-        <Service />
-        <Service />
-        <Service />
-        <Service />
-        <Service />
-        <Service />
-        <Service />
-        <Service />
+        {services && services.map((service) => <Service key={service.name} service={service} />)}
+
       </div>
     );
   }
 }
 
-export default Services;
+Services.propTypes = {
+  services: PropTypes.instanceOf(Array).isRequired,
+  fetchAllServices: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({ services: state.allServices.services });
+
+export default connect(mapStateToProps, Actions)(Services);
