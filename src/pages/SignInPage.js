4d59056b26ec9890aa2f1ecfd11f1a51894cb719
Amita-Roy <sardana.amita2@gmail.com>
2021-04-26 16:08:03 +0200

    Add link to sign up page

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,4 +1,5 @@
 import SignInForm from 'container/SignInForm';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 const SignInPage = () => (
@@ -6,6 +7,12 @@ const SignInPage = () => (
     <div className="px-20 py-16 bg-white" style={{ maxWidth: '800px' }}>
       <h1 className="text-4xl mb-14">Sign In</h1>
       <SignInForm />
+      <p className="mt-24 text-center">
+        New to Encapture?
+        <Link to="/signup" className="ml-2 text-primary font-semibold text-lg">
+          Create an account
+        </Link>
+      </p>
     </div>
   </div>
 );
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,8 +1,19 @@
+import SignUpForm from 'container/SignUpForm';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => (
   <div className="bg-primary bg-opacity-20">
-    <h1>Sign up Page</h1>
+    <div className="px-20 py-16 bg-white" style={{ maxWidth: '800px' }}>
+      <h1 className="text-4xl mb-14">Sign Up</h1>
+      <SignUpForm />
+      <p className="mt-24 text-center">
+        Already have an account ?
+        <Link to="/signin" className="ml-2 text-primary font-semibold text-lg">
+          Sign in
+        </Link>
+      </p>
+    </div>
   </div>
 );
 
