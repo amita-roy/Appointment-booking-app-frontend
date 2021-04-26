99c88d468ecbd65928aa900c3c44996d123f8383
Amita-Roy <sardana.amita2@gmail.com>
2021-04-26 17:55:46 +0200

    Add services reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import allServices from './servicesReducer';
 
 export default combineReducers({
   auth,
+  allServices,
 });
---
+++ src/reducers/servicesReducer.js
@@ -0,0 +1,17 @@
+import ACTIONS from '../actions/actionTypes';
+
+const INITIAL_STATE = { services: [], errors: [] };
+
+const services = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ACTIONS.FETCH_SERVICES:
+      return { ...state, services: action.payload };
+    case ACTIONS.SERVICE_ERR:
+      return { ...state, errors: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export default services;
