f82163aa1e7ce091c02e7c176b4534d2594271e0
Amita-Roy <sardana.amita2@gmail.com>
2021-04-27 21:25:10 +0200

    Created a cart component

---
+++ src/container/Cart.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import SelectedServices from './SelectedServices';
+
+class Cart extends PureComponent {
+  render() {
+    return (
+      <div>
+        <SelectedServices />
+      </div>
+    );
+  }
+}
+
+export default Cart;
