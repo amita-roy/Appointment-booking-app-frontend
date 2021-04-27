464ab139557d73ef91c76976c83b20e73f803ffa
Amita-Roy <sardana.amita2@gmail.com>
2021-04-27 14:10:37 +0200

    Add modal for service detailed view

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,60 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Service = (props) => {
-  const { service } = props;
-  return (
-    <div className="w-80 border-2 py-4 px-5 lg:w-96 border-opacity-50 border-black shadow-xl bg-white cursor-pointer transition duration-300 hover:transform hover:scale-105">
-      <div className="flex justify-center image-wrapper">
-        <div className="h-36 w-36 object-cover object-center rounded-full overflow-hidden">
-          <img src={service.image} alt="skin care" className="h-full w-auto" />
-        </div>
-      </div>
-      <div className="card-content mt-3 ">
-        <div className="top-content flex justify-between items-start">
-          <div className="top-left-content">
-            <p className="title text-xl">{service.name}</p>
-            <p className="description text-sm text-opacity-40 text-black">
-              {service.description}
-            </p>
-          </div>
-          <p className="price text-xl">{`${service.price} SEK`}</p>
-        </div>
+class Service extends Component {
+  handleClick = () => {
+    const { service, openModal } = this.props;
+    openModal(service);
+  };
 
-        <div className="bottom-content flex justify-between items-end mt-2">
-          <div className="tags flex">
-            {service.offer > 0 && (
-              <p className="px-2 pt-2 pb-1 bg-primary mr-3 text-sm offer border border-opacity-30 border-black">
-                {`${service.offer}% Off`}
+  render() {
+    const { service } = this.props;
+    return (
+      <div className="w-80 border-2 py-4 px-5 lg:w-96 border-opacity-50 border-black shadow-xl bg-white cursor-pointer transition duration-300 hover:transform hover:scale-105">
+        <div className="flex justify-center image-wrapper">
+          <div className="h-36 w-36 object-cover object-center rounded-full overflow-hidden">
+            <img
+              src={service.image}
+              alt="skin care"
+              className="h-full w-auto"
+            />
+          </div>
+        </div>
+        <div className="card-content mt-3 ">
+          <div className="top-content flex justify-between items-start">
+            <div className="top-left-content">
+              <p className="title text-xl">{service.name}</p>
+              <p className="description text-sm text-opacity-40 text-black">
+                {service.description}
               </p>
-            )}
-
-            <p className="px-2 pt-2 pb-1 duration border border-opacity-30 border-black text-sm">
-              {`${service.duration} mins`}
-            </p>
+            </div>
+            <p className="price text-xl">{`${service.price} SEK`}</p>
           </div>
-          <button type="submit" className="flex items-center uppercase">
-            View
-            <span className="ml-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </button>
+
+          <div className="bottom-content flex justify-between items-end mt-2">
+            <div className="tags flex">
+              {service.offer > 0 && (
+                <p className="px-2 pt-2 pb-1 bg-primary mr-3 text-sm offer border border-opacity-30 border-black">
+                  {`${service.offer}% Off`}
+                </p>
+              )}
+
+              <p className="px-2 pt-2 pb-1 duration border border-opacity-30 border-black text-sm">
+                {`${service.duration} mins`}
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="flex items-center uppercase focus:outline-none"
+              onClick={this.handleClick}
+            >
+              View
+              <span className="ml-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Service.propTypes = {
   service: PropTypes.shape({
@@ -65,6 +80,7 @@ Service.propTypes = {
     offer: PropTypes.string,
     image: PropTypes.string,
   }).isRequired,
+  openModal: PropTypes.func.isRequired,
 };
 
 export default Service;
--- src/container/Services.js
+++ src/container/Services.js
@@ -3,20 +3,50 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import Modal from 'components/Modal';
 import * as Actions from 'actions';
 
 class Services extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+      clickedService: null,
+    };
+  }
+
   componentDidMount() {
     const { fetchAllServices } = this.props;
     fetchAllServices();
   }
 
+  onOpenModal = (service) => {
+    this.setState({ isModalOpen: true, clickedService: service });
+  };
+
+  onCloseModal = () => {
+    this.setState({ isModalOpen: false, clickedService: null });
+  };
+
   render() {
     const { services } = this.props;
+    const { isModalOpen, clickedService } = this.state;
     return (
       <div className="grid grid-cols-2 xl:grid-cols-3 gap-12">
-        {services && services.map((service) => <Service key={service.name} service={service} />)}
-
+        {services
+          && services.map((service) => (
+            <Service
+              openModal={this.onOpenModal}
+              key={service.name}
+              service={service}
+            />
+          ))}
+        <Modal
+          isOpen={isModalOpen}
+          onClose={this.onCloseModal}
+          service={clickedService}
+        />
       </div>
     );
   }
