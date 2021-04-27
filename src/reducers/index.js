aeb5ac83b5aa3030dc2939c8e7f06f3d378d064c
Amita-Roy <sardana.amita2@gmail.com>
2021-04-27 19:45:36 +0200

    Import selected services reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import allServices from './servicesReducer';
+import selectedServices from './selectedServices';
 
 export default combineReducers({
   auth,
   allServices,
+  selectedServices,
 });
