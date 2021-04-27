a9638173457ae679bef9197c342198ed73be7feb
Amita-Roy <sardana.amita2@gmail.com>
2021-04-27 10:59:35 +0200

    Remove reserve button

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -33,11 +33,22 @@ const Service = (props) => {
               {`${service.duration} mins`}
             </p>
           </div>
-          <button
-            type="button"
-            className="bg-black text-white px-4 py-2 shadow-md focus:outline-none active:bg-opacity-80"
-          >
-            Reserve
+          <button type="submit" className="flex items-center uppercase">
+            View
+            <span className="ml-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
           </button>
         </div>
       </div>
