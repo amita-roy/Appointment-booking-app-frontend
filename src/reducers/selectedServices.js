608f60cb9b91e85a02b2089107f38c59e37b8be0
Amita-Roy <sardana.amita2@gmail.com>
2021-04-28 20:10:50 +0200

    Update the reducer for reset selected services action

--- src/reducers/selectedServices.js
+++ src/reducers/selectedServices.js
@@ -6,6 +6,8 @@ const selectedServices = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTIONS.SERVICE_SELECTED:
       return [...state, action.payload];
+    case ACTIONS.RESET_SELECTED_SERVICES:
+      return INITIAL_STATE;
     default:
       return state;
   }
