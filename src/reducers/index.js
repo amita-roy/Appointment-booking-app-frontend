2e3e1f0cd558ece23ce52b0054b13af8b3a84b3f
Amita-Roy <sardana.amita2@gmail.com>
2021-04-28 17:30:33 +0200

    Import appointments reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import allServices from './servicesReducer';
 import selectedServices from './selectedServices';
+import appointments from './appointments';
 
 export default combineReducers({
   auth,
   allServices,
   selectedServices,
+  allAppointments: appointments,
 });
