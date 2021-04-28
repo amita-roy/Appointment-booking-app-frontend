03cc55df943d89f3867e8953b5df9350055fc894
Amita-Roy <sardana.amita2@gmail.com>
2021-04-28 11:55:19 +0200

    Add minimum height to cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,7 +2,7 @@ import Cart from 'container/Cart';
 import React from 'react';
 
 const CartPage = () => (
-  <div>
+  <div className="border-2" style={{ minHeight: 'calc(100vh - 608px)' }}>
     <h1>Cart</h1>
     <Cart />
   </div>
