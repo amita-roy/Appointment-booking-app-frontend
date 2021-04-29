fbbbf6cc6f136e2f1eccc220d1fb2e8276552970
Amita-Roy <sardana.amita2@gmail.com>
2021-04-29 16:19:58 +0200

    Completed appointments page

--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -8,19 +8,26 @@ const Appointment = (props) => {
 
   const getServices = (ids, services) => ids.map((id) => services.find((s) => id.id === s.id));
   return (
-    <div
-      className="mt-10 bg-white py-8 px-16 shadow-2xl"
-    >
-      <p>{`Kr ${attributes['total-amount']}`}</p>
-      <p>{`Total Services ${data.length}`}</p>
-      <p>{attributes.date}</p>
-      <p>{attributes.time}</p>
-      {getServices(data, includedServices).map((service) => (
-        <div key={service.attributes.name}>
-          <p>{service.attributes.name}</p>
-          <p>{`${service.attributes.duration} mins`}</p>
+    <div className="mt-10 bg-white py-8 px-16 shadow-2xl">
+      <div className="flex justify-between">
+        <div>
+          {getServices(data, includedServices).map((service) => (
+            <div key={service.attributes.name} className="flex">
+              <p className="w-52 lg:w-80">{service.attributes.name}</p>
+              <p className="ml-4">{`${service.attributes.duration} mins`}</p>
+            </div>
+          ))}
         </div>
-      ))}
+        <div>
+          <p className="text-right">{attributes.time}</p>
+          <p className="text-right">{attributes.date}</p>
+        </div>
+      </div>
+      <div className="mt-8 flex justify-between items-center">
+        <p className="text-xl">{`Total Services ${data.length}`}</p>
+        <p className="text-xl text-right">{`Total Amount Kr ${attributes['total-amount']}`}</p>
+      </div>
+      <small className="text-black text-opacity-60">Pay at location.</small>
     </div>
   );
 };
