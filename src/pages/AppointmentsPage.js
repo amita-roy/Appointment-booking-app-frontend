40c6958de61c31d24979049e460b86a86d75ccfa
Amita-Roy <sardana.amita2@gmail.com>
2021-04-29 10:27:50 +0200

    Update the minimum height for Appointments page

--- src/pages/AppointmentsPage.js
+++ src/pages/AppointmentsPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RequireAuth from 'components/requiredAuth';
 
 const AppointmentsPage = () => (
-  <div>
+  <div style={{ minHeight: 'calc(100vh - 608px)' }}>
     <h1>My Appointments</h1>
   </div>
 );
