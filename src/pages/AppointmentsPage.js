dfc26059a31377003ab79130b8e75b11337ffc05
Amita-Roy <sardana.amita2@gmail.com>
2021-04-29 11:54:22 +0200

    Update Appointments page with appointments component import

--- src/pages/AppointmentsPage.js
+++ src/pages/AppointmentsPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import RequireAuth from 'components/requiredAuth';
+import Appointments from 'container/Appointments';
 
 const AppointmentsPage = () => (
   <div style={{ minHeight: 'calc(100vh - 608px)' }}>
     <h1>My Appointments</h1>
+    <Appointments />
   </div>
 );
 
