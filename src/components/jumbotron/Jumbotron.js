1b03aa182a2c45521c659bff9debdd170c1561d3
Amita-Roy <sardana.amita2@gmail.com>
2021-04-30 10:10:40 +0200

    Add classes to jumbotron

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -8,14 +8,14 @@ const Jumbotron = () => (
   >
     <div className="content absolute w-full flex items-center justify-center h-full bg-black bg-opacity-50">
       <div>
-        <h1 className="text-white text-5xl lg:text-6xl ">
+        <h1 className="text-white text-5xl lg:text-6xl title">
           Award-Winning beauty salon
           <span className="block mt-8">in Stockholm</span>
         </h1>
         <Link
           to="/services"
           type="button"
-          className="uppercase bg-white px-6 pt-3 pb-2 text-sm mt-20 focus:outline-none shadow-xl active:bg-opacity-80"
+          className="CTA uppercase bg-white px-6 pt-3 pb-2 text-sm mt-20 focus:outline-none shadow-xl active:bg-opacity-80"
         >
           book now
         </Link>
