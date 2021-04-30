f1c994cc6f2f7424de9772df1c21baee8cf8c5c9
Amita-Roy <sardana.amita2@gmail.com>
2021-04-30 14:19:37 +0200

    Add minimum-height for services page

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Services from 'container/Services';
 
 const ServicesPage = () => (
-  <div className="services-page p-10 flex justify-center bg-primary bg-opacity-20">
+  <div
+    className="services-page p-10 flex justify-center bg-primary bg-opacity-20"
+    style={{ minHeight: 'calc(100vh - 608px)' }}
+  >
     <Services />
-
   </div>
 );
 
