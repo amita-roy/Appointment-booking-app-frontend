bed190744fadbfb39977a2d3e0146f04548824dd
Amita-Roy <sardana.amita2@gmail.com>
2021-05-04 12:59:18 +0200

    Add errors reducer

---
+++ src/reducers/errors.js
@@ -0,0 +1,18 @@
+import ACTIONS from '../actions/actionTypes';
+
+const INITIAL_STATE = [];
+
+const errors = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ACTIONS.SET_ERROR:
+      return action.payload;
+    case ACTIONS.AUTH_USER:
+    case ACTIONS.CREATE_APPOINTMENT:
+      return INITIAL_STATE;
+
+    default:
+      return state;
+  }
+};
+
+export default errors;
