1913ef68a38a2a6a3d51363dc0f026acbadb3168
Amita-Roy <sardana.amita2@gmail.com>
2021-05-04 12:59:02 +0200

    Update authreducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,18 +2,15 @@ import ACTIONS from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   token: '',
-  user: {},
-  errors: null,
+  user: null,
 };
 
 const auth = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTIONS.AUTH_USER:
       return { ...state, token: action.payload, user: action.user };
-    case ACTIONS.ERR:
-      return { ...state, errors: action.payload };
-    case ACTIONS.RESET:
-      return { ...state, errors: null };
+    case ACTIONS.LOGOUT_USER:
+      return INITIAL_STATE;
 
     default:
       return state;
