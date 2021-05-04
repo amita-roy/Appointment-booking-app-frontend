ab46c51a229d35da6ee924b2d1a491728cc7a684
Amita-Roy <sardana.amita2@gmail.com>
2021-05-04 08:12:47 +0200

    Update root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import allServices from './servicesReducer';
-import selectedServices from './selectedServices';
 import appointments from './appointments';
 
 export default combineReducers({
   auth,
   allServices,
-  selectedServices,
   allAppointments: appointments,
 });
