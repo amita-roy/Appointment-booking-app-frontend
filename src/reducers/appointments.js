f1ce72a195481714b4c3de4fc056db8ce0b5043a
Amita-Roy <sardana.amita2@gmail.com>
2021-05-04 12:58:47 +0200

    Update appointments reducer

--- src/reducers/appointments.js
+++ src/reducers/appointments.js
@@ -1,11 +1,11 @@
 import ACTIONS from '../actions/actionTypes';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = [];
 
 const appointments = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTIONS.FETCH_APPOINTMENTS:
-      return { ...state, appointments: action.payload.data };
+      return action.payload;
     default:
       return state;
   }
